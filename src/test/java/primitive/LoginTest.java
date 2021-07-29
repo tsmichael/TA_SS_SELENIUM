d9df47353b17a313d875dd119bbe556d18f5811c
Mykhailo <pmtsmichael@gmail.com>
2021-07-29 16:42:34 +0300

    added 2nd auto test - verifyUserCanNotLoginWithWrongPassword

--- src/test/java/primitive/LoginTest.java
+++ src/test/java/primitive/LoginTest.java
@@ -45,4 +45,37 @@ public class LoginTest {
 
         driver.quit();
     }
+
+    @Test
+    public void verifyUserCanNotLoginWithWrongPassword(){
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://training.epam.ua");
+
+        WebElement signInButton = driver.findElement(By.className("header-auth__signin"));
+        signInButton.click();
+
+        WebElement emailInput = driver.findElement(By.id("username"));
+        emailInput.sendKeys(jsonDataConfig.getEmailFromJson(1));
+
+        WebElement continueButton = driver.findElement(By.id("kc-login-next"));
+        continueButton.click();
+
+        WebElement passwordInput = driver.findElement(By.id("password"));
+        passwordInput.sendKeys(jsonDataConfig.getPasswordFromJson(1));
+
+        WebElement signInButtonForLogin = driver.findElement(By.id("kc-login"));
+        signInButtonForLogin.click();
+
+        WebElement errorMessage = driver.findElement(By.className("error-text"));
+        String errorMessageText =errorMessage.getAttribute("innerHTML");
+        String expectedError = "We can't find user with such credentials.";
+        Assert.assertEquals(errorMessageText,expectedError, "error message NOT displayed - something went wrong");
+
+
+        driver.quit();
+    }
 }
