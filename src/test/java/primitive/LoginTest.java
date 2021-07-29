38dda4a1ba138ebfabe446cc26c5072d23924293
Mykhailo <pmtsmichael@gmail.com>
2021-07-29 16:16:12 +0300

    added 1st positive auto test - verifyUserIsSuccessfullyLoggedIn

---
+++ src/test/java/primitive/LoginTest.java
@@ -0,0 +1,48 @@
+package primitive;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import tools.JSONDataConfig;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginTest {
+    JSONDataConfig jsonDataConfig = new JSONDataConfig("src/main/resources/data.json");
+
+
+    @Test
+    public void verifyUserIsSuccessfullyLoggedIn(){
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://training.epam.ua");
+
+        WebElement signInButton = driver.findElement(By.className("header-auth__signin"));
+        signInButton.click();
+
+        WebElement emailInput = driver.findElement(By.id("username"));
+        emailInput.sendKeys(jsonDataConfig.getEmailFromJson(0));
+
+        WebElement continueButton = driver.findElement(By.id("kc-login-next"));
+        continueButton.click();
+
+        WebElement passwordInput = driver.findElement(By.id("password"));
+        passwordInput.sendKeys(jsonDataConfig.getPasswordFromJson(0));
+
+        WebElement signInButtonForLogin = driver.findElement(By.id("kc-login"));
+        signInButtonForLogin.click();
+
+        WebElement userInfoName = driver.findElement(By.className("user-info__name"));
+
+        Assert.assertTrue(userInfoName.isDisplayed());
+
+
+        driver.quit();
+    }
+}
