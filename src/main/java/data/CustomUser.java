3c2d0fa0a58e24c18bb76cd6c33abeb0a58cd6c1
Mykhailo <pmtsmichael@gmail.com>
2021-07-29 15:10:40 +0300

    added CustomUser and JsonDataConfig

---
+++ src/main/java/data/CustomUser.java
@@ -0,0 +1,15 @@
+package data;
+
+public class CustomUser {
+
+    String email;
+    String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
---
+++ src/main/java/tools/JSONDataConfig.java
@@ -0,0 +1,33 @@
+package tools;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import data.CustomUser;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class JSONDataConfig {
+
+    private List<CustomUser> listUser;
+
+    public JSONDataConfig(String fileName) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        listUser = null;
+        try {
+            listUser = objectMapper.readValue(new File(fileName), new TypeReference<List<CustomUser>>() {
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getEmailFromJson(int user_index){
+        return listUser.get(user_index).getEmail();
+    }
+
+    public String getPasswordFromJson(int user_index) {
+        return listUser.get(user_index).getPassword();
+    }
+}
