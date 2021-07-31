9554cd73fa2371b5b87c878ce606e9563cf6c89f
Mykhailo <pmtsmichael@gmail.com>
2021-07-31 17:23:39 +0300

    added Driver class

--- src/main/java/constants/DriverConfig.java
+++ src/main/java/constants/DriverConfig.java
@@ -3,12 +3,26 @@ package constants;
 public enum DriverConfig {
 
     CHROME_NAME("webdriver.chrome.driver"),
-    CHROME_DRIVER_LOCATION("src/main/resources/chromedriver.exe");
+    CHROME_DRIVER_LOCATION("src/main/resources/chromedriver.exe"),
+    IMPLICITLY_WAIT(10);
 
     String path;
+    long seconds;
 
     DriverConfig(String path) {
         this.path = path;
+    }
+
+    DriverConfig(long seconds) {
+        this.seconds = seconds;
+    }
+
+    public String getPath() {
+        return this.path;
+    }
+
+    public long getSeconds() {
+        return this.seconds;
     }
 
     @Override
---
+++ src/main/java/driver/CustomDriver.java
@@ -0,0 +1,34 @@
+package driver;
+
+import constants.DriverConfig;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class CustomDriver {
+
+    private static WebDriver webDriver;
+
+    protected void initDriver(final String browserName) {
+
+        if (DriverConfig.CHROME_NAME.equals(browserName)) {
+            System.setProperty(DriverConfig.CHROME_NAME.getPath(), DriverConfig.CHROME_DRIVER_LOCATION.getPath());
+            webDriver = new ChromeDriver();
+        }
+
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(DriverConfig.IMPLICITLY_WAIT.getSeconds(), TimeUnit.SECONDS);
+    }
+
+    public static WebDriver getDriver() {
+        return webDriver;
+    }
+
+    protected void quitDriver() {
+        if (webDriver != null) {
+            webDriver.quit();
+            webDriver = null;
+        }
+    }
+}
