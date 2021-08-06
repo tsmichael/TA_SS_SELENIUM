6a2c9e1ce410ddac825ad7470eecfc10ffba68cc
Mykhailo <pmtsmichael@gmail.com>
2021-08-06 21:07:31 +0300

    refactor CustomDriver according to Singleton pattern

--- src/main/java/driver/CustomDriver.java
+++ src/main/java/driver/CustomDriver.java
@@ -7,20 +7,26 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
-public abstract class CustomDriver {
+public class CustomDriver {
+
+    private static CustomDriver instance = null;
 
     private static WebDriver webDriver;
 
-    public static void initDriver(DriverConfig driverConfig) {
-        if (webDriver == null) {
+    private CustomDriver() {
+    }
+
+    public static CustomDriver initDriver(DriverConfig driverConfig) {
+        if (instance == null) {
+            instance = new CustomDriver();
             if (DriverConfig.CHROME_NAME.equals(driverConfig)) {
                 System.setProperty(DriverConfig.CHROME_NAME.getPath(), DriverConfig.CHROME_DRIVER_LOCATION.getPath());
                 webDriver = new ChromeDriver();
             }
-
             webDriver.manage().window().maximize();
             webDriver.manage().timeouts().implicitlyWait(ConstantConfig.IMPLICITLY_WAIT.getSeconds(), TimeUnit.SECONDS);
         }
+        return instance;
     }
 
     public static WebDriver getDriver() {
@@ -31,6 +37,7 @@ public abstract class CustomDriver {
         if (webDriver != null) {
             webDriver.quit();
             webDriver = null;
+            instance = null;
         }
     }
 }
